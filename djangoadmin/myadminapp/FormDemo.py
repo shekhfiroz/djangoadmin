5d851e470812aa74a5b8cc1cb28f67747a04871a
sfa <firoz>
2018-01-19 12:27:48 +0530

    Formvia class

--- djangoadmin/myadminapp/FormDemo.py
+++ djangoadmin/myadminapp/FormDemo.py
@@ -2,7 +2,12 @@ from django import forms
 #writing a contactus form of a website
 
 class ContactusForm(forms.Form):
-    fname=forms.CharField()
-    lname=forms.CharField()
-    email=forms.EmailField()
-    message=forms.CharField(required=False)
+
+    def formcontent(self):
+        fname=forms.CharField()
+        lname=forms.CharField()
+        email=forms.EmailField()
+        message=forms.CharField(required=False)
+
+contactusform=ContactusForm()
+contactusform.formcontent()
